ce347af5329e657d01374bf0a0e5a9aec6347817
Hack3r <Hack3r@Hack3r-PC>
2012-06-04 18:58:00 +0300

    Invite Answer implemented.

--- src/commands/customGame/InviteAnswer.java
+++ src/commands/customGame/InviteAnswer.java
@@ -1,16 +1,46 @@
 package commands.customGame;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.TreeSet;
+
+import javax.swing.plaf.ListUI;
+
+import tablePackage.CustomTable;
+import tablePackage.Table;
+import tablePackage.TableManager;
 import clientPackage.Client;
 import commands.Command;
 
 public class InviteAnswer extends Command {
 
+	/**
+	 * • param[0] : command string
+	 * • param[1] : table name
+	 * • param[2] : reply 
+	 */
+	
 	private static boolean enabled = true;
 	
 	@Override
 	public void execute(Client client, String... params) {
-		// TODO Auto-generated method stub
-
+		if (!InviteAnswer.enabled)
+			return;
+		
+		CustomTable table = TableManager.getCustomTable(params[1]);
+		boolean reply = Boolean.parseBoolean(params[2]);
+		
+		List<Client> clients = table.getAllClients();
+		for (Client c : clients)
+			client.send("3hR~" + reply + "~");
+		
+		if (reply == true){
+			for (Client c : clients){
+				client.send("3c1R~" + reply + "~");
+				if (c != client)
+					client.send("3c2R~" + reply + "~");
+			}
+		}
 	}
 
 	@Override
